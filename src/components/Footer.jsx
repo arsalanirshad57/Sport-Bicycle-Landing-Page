c0babc2adfce1040e249c17c187a66e2162b6e09
Arsalan Irshad <140166441+arsalanirshad57@users.noreply.github.com>
2024-05-22 16:12:12 +0500

    minor fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Span from './Span'
 const Footer = () => {
   return (
-    <div className=' flex justify-center items-center w-full h-full py-2 bg-rgbBlack '>
+    <div className=' flex justify-center items-center w-full h-full py-2 bg-rgbBlack mt-2 xl:mt-0 '>
         <Span className='!text-base md:!text-lg'>&copy; Sport-Bicycle all rights reserved.</Span>
     </div>
   )
